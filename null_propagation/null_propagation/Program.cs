005c803782b98b5d5641c1a48f403b19ecbdbc60
bhargavak <bhargavak37@gmail.com>
2017-08-20 11:06:03 +0530

    null_propagation in C#
    null propagation , null types, compiler check null types, null pointer exceptions

---
+++ null_propagation/null_propagation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace null_propagation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x =123;
+            double? y=456.7;
+            double? s = x + y;
+
+            x = null;
+            s = x + y;
+            bool hasValue = x.HasValue;
+            string text = x.ToString();
+            int valueOrDefault1 = x.GetValueOrDefault();
+            int valueorDefault2 = x.GetValueOrDefault(10);  
+            int value;
+          //  value = x.Value;
+            if (y != null)
+                Console.WriteLine("not null");
+            if (y.HasValue)
+                Console.WriteLine("not null");
+            value = x ?? 10;
+            x = 123;
+            value = x ?? 10;
+            string firstName = "Madonna";
+            string secondName = null;
+            string name = firstName + " " + (secondName ?? "[no last name]");
+            name = firstName + " " + secondName ?? "[no last  name]";
+            name = $"{firstName} {secondName ?? "[no last name]"}";
+            //int? length = secondName?.Length;
+            //int? length = (secondName == null) ? (int?)null : secondName.Length;
+            DateTime? date = null;
+            DayOfWeek? dow = date?.DayOfWeek;
+        }
+       
+    }
+}
